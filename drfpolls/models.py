37a46d704f2feb5319b1b878fdbd29398a1525ca
nanase.shimoyama <nanase.shimoyama@visasq.com>
2023-03-23 19:06:30 +0900

    “Choice/CommentのmodelにForeignKeyを設定”

--- drfpolls/models.py
+++ drfpolls/models.py
@@ -15,15 +15,17 @@ class Question(models.Model):
 
 class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField()
-    question_id = models.IntegerField()
+    votes = models.IntegerField(default=0)
+    question = models.ForeignKey(Question, null=False, on_delete=models.CASCADE)
+    # migrationを詳しく追ってみる mypy isort black bandit
     
     def __str__(self):
         return self.choice_text
 
 class Comment(models.Model):
-    comment_text = models.TextField(max_length=300)
+    comment_text = models.TextField(max_length=300, null=False,)
     comment_date = models.DateTimeField('date published')
+    question = models.ForeignKey(Question, null=False, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.comment_text

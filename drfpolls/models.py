cbcf3b767a84580d68f015a685e7b37254212536
nanase.shimoyama <nanase.shimoyama@visasq.com>
2023-03-28 19:26:33 +0900

    “コメントがcreateできない事象を修正”

--- drfpolls/models.py
+++ drfpolls/models.py
@@ -17,7 +17,6 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
     question = models.ForeignKey(Question, null=False, on_delete=models.CASCADE)
-    # migrationを詳しく追ってみる mypy isort black bandit
     
     def __str__(self):
         return self.choice_text
